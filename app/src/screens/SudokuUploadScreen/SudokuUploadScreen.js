28ff94c24803263d3bbc87428cecf1ba7120a06e
rz03 <tushar.code.inbox@gmail.com>
2023-11-21 21:14:35 +0530

    [feat]: made sudoko upload screen

--- app/src/screens/SudokuUploadScreen/SudokuUploadScreen.js
+++ app/src/screens/SudokuUploadScreen/SudokuUploadScreen.js
@@ -1,10 +1,75 @@
-import React from "react";
-import { Text, View } from "react-native";
-
+// src/screens/SudokuUploadScreen/SudokuUploadScreen.js
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Alert,
+  ImageBackground,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import Button from "../../components/Button";
+import OutlineButton from "../../components/OutlineButton";
 const SudokuUploadScreen = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const selectImageFromGallery = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setSelectedImage(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.error("Error selecting image from gallery:", error);
+    }
+  };
+
+  const takeImageFromCamera = async () => {
+    try {
+      const result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setSelectedImage(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.error("Error taking image from camera:", error);
+    }
+  };
+
   return (
-    <View>
-      <Text>SudokuUploadScreen</Text>
+    <View className="bg-background h-full flex flex-col justify-center items-center">
+      <View className="w-full p-10">
+        <Text className="text-3xl font-bold mb-2 text-foreground">
+          Greetings, Puzzler!
+        </Text>
+        <Text className="text-mutedForeground">
+          Upload your Sudoku and let the magic unfold!
+        </Text>
+      </View>
+      <View className="flex flex-row justify-evenly w-full mb-4 mt-32">
+        <Button title="Upload from Gallery" onPress={selectImageFromGallery} />
+        <OutlineButton title="Take Photo" onPress={takeImageFromCamera} />
+      </View>
+      {selectedImage ? (
+        <ImageBackground
+          source={{ uri: selectedImage }}
+          className="h-200 w-200 mb-4"
+          resizeMode="contain"
+        />
+      ) : (
+        // <Text className="mb-4 text-foreground">No image selected</Text>
+        ""
+      )}
     </View>
   );
 };
