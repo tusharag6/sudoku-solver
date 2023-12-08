b9fe5c0603a13c09ac9aba44c5e91db51b303b60
rz03 <tushar.code.inbox@gmail.com>
2023-12-09 00:22:08 +0530

    refactor: added missing model and removed redundant files

--- ml/Utility_Func.py
+++ ml/Utility_Func.py
@@ -1,17 +1,19 @@
 import cv2
 import numpy as np
-from tensorflow.keras.models import load_model
+from tensorflow import keras
+# from tensorflow.keras.models import load_model
 
 
 def intializePredectionModel():
-    model = load_model('models/myModel.h5')
+    model = keras.models.load_model('models/digit_model_nn.h5')
     return model
 
 
 def preProcess(img):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
-    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, 1, 1, 11, 2)  # APPLY ADAPTIVE THRESHOLD
+    imgThreshold = cv2.adaptiveThreshold(
+        imgBlur, 255, 1, 1, 11, 2)  # APPLY ADAPTIVE THRESHOLD
     return imgThreshold
 
 
@@ -76,7 +78,8 @@ def displayNumbers(img, numbers, color=(0, 255, 0)):
         for y in range(0, 9):
             if numbers[(y * 9) + x] != 0:
                 cv2.putText(img, str(numbers[(y * 9) + x]),
-                            (x * secW + int(secW / 2) - 10, int((y + 0.8) * secH)), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                            (x * secW + int(secW / 2) - 10, int((y + 0.8)
+                             * secH)), cv2.FONT_HERSHEY_COMPLEX_SMALL,
                             2, color, 2, cv2.LINE_AA)
     return img
 
@@ -103,8 +106,11 @@ def stackImages(imgArray, scale):
     if rowsAvailable:
         for x in range(0, rows):
             for y in range(0, cols):
-                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+                imgArray[x][y] = cv2.resize(
+                    imgArray[x][y], (0, 0), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(
+                        imgArray[x][y], cv2.COLOR_GRAY2BGR)
         imageBlank = np.zeros((height, width, 3), np.uint8)
         hor = [imageBlank] * rows
         hor_con = [imageBlank] * rows
@@ -116,7 +122,8 @@ def stackImages(imgArray, scale):
     else:
         for x in range(0, rows):
             imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
         hor = np.hstack(imgArray)
         hor_con = np.concatenate(imgArray)
         ver = hor
